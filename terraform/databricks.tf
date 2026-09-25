e40651e7d8f7d8c7dbbcb94e9ba02da9902791ff
Matthew Lau <lau.cy.matthew@gmail.com>
2026-09-25 15:11:45 +0100

    fi:x node size

--- terraform/databricks.tf
+++ terraform/databricks.tf
@@ -1,22 +1,19 @@
+# Retrieve latest LTS Spark runtime and single-node/multi-node instance types
 data "databricks_spark_version" "latest_lts" {
   long_term_support = true
+  depends_on        = [azurerm_databricks_workspace.this]
 }
 
-data "databricks_node_type" "cheapest" {
-  min_cores   = 4
-  gb_per_core = 4
-}
-
-# Single-Node or Multi-Node ETL Compute Cluster
 resource "databricks_cluster" "etl_cluster" {
   cluster_name            = "etl-processing-cluster"
   spark_version           = data.databricks_spark_version.latest_lts.id
-  node_type_id            = data.databricks_node_type.cheapest.id
-  autotermination_minutes = 20 # Aggressive auto-termination
+  node_type_id            = "Standard_DS3_v2"
+  driver_node_type_id     = "Standard_DS3_v2"
+  autotermination_minutes = 20 # Terminate to reduce idle cost
 
-  spark_conf = {
-    "spark.databricks.cluster.profile" = "singleNode"
-    "spark.master"                     = "local[*]"
+  autoscale {
+    min_workers = 1
+    max_workers = 1
   }
 }
 
@@ -42,7 +39,7 @@ resource "databricks_job" "etl_workflow" {
     job_cluster_key = "job_cluster"
     new_cluster {
       spark_version = data.databricks_spark_version.latest_lts.id
-      node_type_id  = data.databricks_node_type.cheapest.id
+      node_type_id  = data.databricks_node_type.smallest.id
       num_workers   = 2
     }
   }

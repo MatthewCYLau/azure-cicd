beae7c1294c38ff3d15a841a4d75ce876b8f28ca
Matthew Lau <lau.cy.matthew@gmail.com>
2026-09-26 12:35:56 +0100

    chore: remove auth type

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,6 @@ provider "azurerm" {
 
 provider "databricks" {
   host                        = "https://${azurerm_databricks_workspace.this.workspace_url}"
-  azure_workspace_resource_id = azurerm_databricks_workspace.this.id
-  auth_type                   = "azure-cli"
+  # azure_workspace_resource_id = azurerm_databricks_workspace.this.id
+  # auth_type                   = "azure-cli"
 }
